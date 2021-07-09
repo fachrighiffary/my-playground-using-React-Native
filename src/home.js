6601c9afd2940580414319b6523754f8fa92aba6
Fachri Ghiffary <fachrighiffary@ruangalternative.com>
2021-07-09 11:36:30 +0700

    renew home view

--- src/home.js
+++ src/home.js
@@ -19,114 +19,130 @@ const Home = ({navigation}) => {
       </SafeAreaView>
 
       {/* Body */}
-      <ScrollView>
-        <TouchableOpacity
-          style={{...styles.btn, backgroundColor: 'lightgrey'}}
-          onPress={() => {
-            navigation.navigate('ToastMessage');
-          }}>
-          <Text style={{color: 'black'}}>Test Toast Message</Text>
-        </TouchableOpacity>
+      <ScrollView style={{paddingHorizontal: 20}}>
+        <View style={styles.flexDir}>
+          <TouchableOpacity
+            style={{...styles.btn, backgroundColor: 'lightgrey'}}
+            onPress={() => {
+              navigation.navigate('ToastMessage');
+            }}>
+            <Text style={{color: 'black'}}>Test Toast Message</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity
-          style={{...styles.btn, backgroundColor: 'lightgrey'}}
-          onPress={() => {
-            navigation.navigate('CameraGalery');
-          }}>
-          <Text style={{color: 'black'}}>Test Camera And Galery</Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            style={{...styles.btn, backgroundColor: 'lightgrey'}}
+            onPress={() => {
+              navigation.navigate('CameraGalery');
+            }}>
+            <Text style={{color: 'black'}}>Test Camera And Galery</Text>
+          </TouchableOpacity>
+        </View>
 
-        <TouchableOpacity
-          style={{...styles.btn, backgroundColor: 'lightgrey'}}
-          onPress={() => {
-            navigation.navigate('Loading');
-          }}>
-          <Text style={{color: 'black'}}>Test Loading</Text>
-        </TouchableOpacity>
+        <View style={styles.flexDir}>
+          <TouchableOpacity
+            style={{...styles.btn, backgroundColor: 'lightgrey'}}
+            onPress={() => {
+              navigation.navigate('Loading');
+            }}>
+            <Text style={{color: 'black'}}>Test Loading</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity
-          style={{...styles.btn, backgroundColor: 'lightgrey'}}
-          onPress={() => {
-            navigation.navigate('TestNotification');
-          }}>
-          <Text style={{color: 'black'}}>Test Notification</Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            style={{...styles.btn, backgroundColor: 'lightgrey'}}
+            onPress={() => {
+              navigation.navigate('TestNotification');
+            }}>
+            <Text style={{color: 'black'}}>Test Notification</Text>
+          </TouchableOpacity>
+        </View>
 
-        <TouchableOpacity
-          style={{...styles.btn, backgroundColor: 'lightgrey'}}
-          onPress={() => {
-            navigation.navigate('CustomFonts');
-          }}>
-          <Text style={{color: 'black'}}>Custom Fonts</Text>
-        </TouchableOpacity>
+        <View style={styles.flexDir}>
+          <TouchableOpacity
+            style={{...styles.btn, backgroundColor: 'lightgrey'}}
+            onPress={() => {
+              navigation.navigate('CustomFonts');
+            }}>
+            <Text style={{color: 'black'}}>Custom Fonts</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity
-          style={{...styles.btn, backgroundColor: 'lightgrey'}}
-          onPress={() => {
-            navigation.navigate('ViewAndZoom');
-          }}>
-          <Text style={{color: 'black'}}>View and zoom Image</Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            style={{...styles.btn, backgroundColor: 'lightgrey'}}
+            onPress={() => {
+              navigation.navigate('ViewAndZoom');
+            }}>
+            <Text style={{color: 'black'}}>View and zoom Image</Text>
+          </TouchableOpacity>
+        </View>
 
-        <TouchableOpacity
-          style={{...styles.btn, backgroundColor: 'lightgrey'}}
-          onPress={() => {
-            navigation.navigate('HeaderAnsena');
-          }}>
-          <Text style={{color: 'black'}}>Header Ansena app</Text>
-        </TouchableOpacity>
+        <View style={styles.flexDir}>
+          <TouchableOpacity
+            style={{...styles.btn, backgroundColor: 'lightgrey'}}
+            onPress={() => {
+              navigation.navigate('HeaderAnsena');
+            }}>
+            <Text style={{color: 'black'}}>Header Ansena app</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity
-          style={{...styles.btn, backgroundColor: 'lightgrey'}}
-          onPress={() => {
-            navigation.navigate('PlayDirection');
-          }}>
-          <Text style={{color: 'black'}}>Play Direction View</Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            style={{...styles.btn, backgroundColor: 'lightgrey'}}
+            onPress={() => {
+              navigation.navigate('PlayDirection');
+            }}>
+            <Text style={{color: 'black'}}>Play Direction View</Text>
+          </TouchableOpacity>
+        </View>
 
-        <TouchableOpacity
-          style={{...styles.btn, backgroundColor: 'lightgrey'}}
-          onPress={() => {
-            navigation.navigate('flatlistCarousel');
-          }}>
-          <Text style={{color: 'black'}}>Flatlist Carousell</Text>
-        </TouchableOpacity>
+        <View style={styles.flexDir}>
+          <TouchableOpacity
+            style={{...styles.btn, backgroundColor: 'lightgrey'}}
+            onPress={() => {
+              navigation.navigate('flatlistCarousel');
+            }}>
+            <Text style={{color: 'black'}}>Flatlist Carousell</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity
-          style={{...styles.btn, backgroundColor: 'lightgrey'}}
-          onPress={() => {
-            navigation.navigate('snapCarousel');
-          }}>
-          <Text style={{color: 'black'}}>SnapCarousel Carousell</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{...styles.btn, backgroundColor: 'lightgrey'}}
-          onPress={() => {
-            navigation.navigate('swipetinder');
-          }}>
-          <Text style={{color: 'black'}}>Swipe Deck Tinder</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{...styles.btn, backgroundColor: 'lightgrey'}}
-          onPress={() => {
-            navigation.navigate('flipcard');
-          }}>
-          <Text style={{color: 'black'}}>Flip Card</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{...styles.btn, backgroundColor: 'lightgrey'}}
-          onPress={() => {
-            navigation.navigate('fadeanimation');
-          }}>
-          <Text style={{color: 'black'}}>Fade In, Fade Out Animation</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{...styles.btn, backgroundColor: 'lightgrey'}}
-          onPress={() => {
-            navigation.navigate('timer');
-          }}>
-          <Text style={{color: 'black'}}>Timer Countdown</Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            style={{...styles.btn, backgroundColor: 'lightgrey'}}
+            onPress={() => {
+              navigation.navigate('snapCarousel');
+            }}>
+            <Text style={{color: 'black'}}>SnapCarousel Carousell</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.flexDir}>
+          <TouchableOpacity
+            style={{...styles.btn, backgroundColor: 'lightgrey'}}
+            onPress={() => {
+              navigation.navigate('swipetinder');
+            }}>
+            <Text style={{color: 'black'}}>Swipe Deck Tinder</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{...styles.btn, backgroundColor: 'lightgrey'}}
+            onPress={() => {
+              navigation.navigate('flipcard');
+            }}>
+            <Text style={{color: 'black'}}>Flip Card</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.flexDir}>
+          <TouchableOpacity
+            style={{...styles.btn, backgroundColor: 'lightgrey'}}
+            onPress={() => {
+              navigation.navigate('fadeanimation');
+            }}>
+            <Text style={{color: 'black'}}>Fade In, Fade Out Animation</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{...styles.btn, backgroundColor: 'lightgrey'}}
+            onPress={() => {
+              navigation.navigate('timer');
+            }}>
+            <Text style={{color: 'black'}}>Timer Countdown</Text>
+          </TouchableOpacity>
+        </View>
       </ScrollView>
     </View>
   );
@@ -154,10 +170,11 @@ const styles = StyleSheet.create({
   btn: {
     alignSelf: 'center',
     borderRadius: 10,
-    height: 40,
-    width: 250,
+    height: 100,
+    width: 150,
     justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 20,
   },
+  flexDir: {flexDirection: 'row', justifyContent: 'space-around'},
 });
